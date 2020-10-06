0602319a4048013f729c97310e6eb62a430a368c
Brian <bdrr77@gmail.com>
2020-10-06 11:16:42 +0200

    Working build, same sizes as SSv5 except for debug sections

--- application.cmake
+++ application.cmake
@@ -5,7 +5,7 @@ target_sources(${PROJECT_NAME}
     
     #Startup and System
     ${SL_TARGET_PART_SOURCE_PATH}/Source/system_${SL_TARGET_PART_LOWER_CASE}.c
-    #${SL_TARGET_PART_SOURCE_PATH}/Source/${SL_TOOLCHAIN}/startup_${SL_TARGET_PART_LOWER_CASE}.S
+    ${SL_TARGET_PART_SOURCE_PATH}/Source/${SL_TOOLCHAIN}/startup_${SL_TARGET_PART_LOWER_CASE}.S
 
     #Emlib
     ${SL_GECKO_SDK_SUITE_PATH}/platform/emlib/src/em_cmu.c
@@ -30,23 +30,14 @@ target_compile_definitions(${PROJECT_NAME}
         -D${SL_TARGET_PART_NO_UPPER_CASE}=1
 )
 
-add_library(${PROJECT_NAME}_ASM 
-  STATIC
-    ${SL_TARGET_PART_SOURCE_PATH}/Source/${SL_TOOLCHAIN}/startup_${SL_TARGET_PART_LOWER_CASE}.S
-  )
-
-target_link_libraries(${PROJECT_NAME} ${PROJECT_NAME}_ASM)
-
 # Print executable size
 add_custom_command(TARGET ${PROJECT_NAME}
         POST_BUILD
-        COMMAND ${SIZE} ${PROJECT_NAME})
+        COMMAND ${SIZE} ${PROJECT_NAME} -A)
 
 # Create hex file
 add_custom_command(TARGET ${PROJECT_NAME}
         POST_BUILD
         COMMAND ${OBJCOPY} -O ihex ${PROJECT_NAME} ${PROJECT_NAME}.hex
         COMMAND ${OBJCOPY} -O binary ${PROJECT_NAME} ${PROJECT_NAME}.bin)
-
-
 
